007a7a7ff7c8293af5ce6297e2d89f407cd28dc2
kyungku.kang <coush123@gmail.com>
2021-01-20 11:03:10 +0900

    취약점 조치2 완료

--- hello.py
+++ hello.py
@@ -10,15 +10,17 @@ from subprocess import Popen
 ######취약점 조치1 - ncpa.cpl실행 > NetBIOS over TCP/IP 사용안함으로 설정(기본값: 0 사용함: 1 사용안함 2)########
 
 #한줄 명령어 코드#
-
+"""
 netBios_unable = '(Get-WmiObject Win32_NetworkAdapterConfiguration -Filter IpEnabled="true").SetTcpipNetbios(2)'
 subprocess.call(["powershell",netBios_unable],shell = True)
+"""
 
 
 def run(cmd):
     completed = subprocess.run(["powershell","-ExecutionPolicy","Bypass",cmd], shell = True)#stdout = subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
     return completed
 
+"""
 if __name__ == '__main__':
 
     tmp = os.getcwd()
@@ -29,10 +31,29 @@ if __name__ == '__main__':
     if netBios_info.returncode != 0:
         print("An error occured: %s", netBios_info.stderr)
     else:
-        print("Hello command executed successfully!")
+        print("취약점 조치1 command executed successfully!")
     
     print("-------------------------")
     
-
+"""
 
 ######취약점 조치2 - C:\Windows\system32\config\SAM파일 우클릭 > 속성 > 보안탭에서 Administrator, System 그룹외 다른 사용자 삭제######
+
+######취약점 조치3 - 로컬보안 정책 > 보안 옵션 > SAM계정 옵션 모두 사용으로 설정######
+
+
+if __name__ == '__main__':
+
+    tmp = os.getcwd()
+    filepath = tmp + "\\samOpt.ps1"
+    sam_info = run(filepath)
+    
+    
+    if sam_info.returncode != 0:
+        print("An error occured: %s", sam_info.stderr)
+    else:
+        print("취약점 조치2 command executed successfully!")
+    
+    print("-------------------------")
+
+######취약점 조치4 - 
